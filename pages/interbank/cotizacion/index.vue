1933dfa9a2a3ce44cebac0fd8ca27e8aed4aec96
Luis Hernandez <luis.hernandez@interseguro.com.pe>
2021-03-18 14:31:56 -0500

    porcentaje ibk

--- pages/interbank/cotizacion/index.vue
+++ pages/interbank/cotizacion/index.vue
@@ -261,7 +261,7 @@
                       </div>
                       <div class="dto-cotizador">
                         <div class="flotante-dcto">
-                          <span>10%</span>
+                          <span>15%</span>
                           
                         </div>
                       </div>
@@ -345,7 +345,7 @@
                       </div>
                       <div class="dto-cotizador">
                         <div class="flotante-dcto">
-                          <span>10%</span>
+                          <span>15%</span>
                           
                         </div>
                       </div>
@@ -446,7 +446,7 @@
                       </div>
                       <div class="dto-cotizador">
                         <div class="flotante-dcto">
-                          <span>10%</span>
+                          <span>15%</span>
                           
                         </div>
                       </div>
@@ -575,7 +575,7 @@
                 </div>
                 <div class="dto-cotizador">
                   <div class="flotante-dcto">
-                    <span>10%</span>
+                    <span>15%</span>
                     
                   </div>
                 </div>
