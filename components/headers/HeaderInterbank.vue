f812e0ed847aa98e570a356bc0c236580efbe7c6
Jorge Sulca <jorge.sulca@qdsgroup.com.pe>
2021-08-17 09:41:08 -0500

    amplia contador

--- components/headers/HeaderInterbank.vue
+++ components/headers/HeaderInterbank.vue
@@ -209,8 +209,8 @@ export default {
     computed: {},
     methods:{
         contador(){
-            var flipdown2 = new FlipDown(1629089999, 'contadorMobile').start()
-            var flipdown = new FlipDown(1629089999, 'contadorDesktop').start()
+            var flipdown2 = new FlipDown(1629694799, 'contadorMobile').start()
+            var flipdown = new FlipDown(1629694799, 'contadorDesktop').start()
         },
         closeListon(){
             document.getElementById("liston-desktop").style.display = "none"
