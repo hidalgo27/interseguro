256f2586511ead0e71758473922f691eee44929e
Jorge Sulca <jorge.sulca@qdsgroup.com.pe>
2021-09-06 11:13:13 -0500

    amplia  campania

--- pages/interbank/index.vue
+++ pages/interbank/index.vue
@@ -458,8 +458,8 @@
       },
       methods: {
         contador(){
-            var flipdown2 = new FlipDown(1630904399, 'contadorCyberDesktop').start()
-            var flipdown = new FlipDown(1630904399, 'contadorCyberMobile').start()
+            var flipdown2 = new FlipDown(1631163599, 'contadorCyberDesktop').start()
+            var flipdown = new FlipDown(1631163599, 'contadorCyberMobile').start()
         },
         pago_datalayer(error_detectado){
           window.dataLayer = window.dataLayer || [];
