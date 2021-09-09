d678b4a58977d69623ec8cd3cd4f3d21461d5150
Jorge Sulca <jorge.sulca@qdsgroup.com.pe>
2021-09-09 10:20:17 -0500

    amplia campaña al 11-09

--- pages/index.vue
+++ pages/index.vue
@@ -448,8 +448,8 @@ import FadeLoader from '@/components/loaders/FadeLoader'
         onSubmit(){
         },
         contador(){
-            var flipdown2 = new FlipDown(1631163599, 'contadorCyberDesktop').start()
-            var flipdown = new FlipDown(1631163599, 'contadorCyberMobile').start()
+            var flipdown2 = new FlipDown(1631422799, 'contadorCyberDesktop').start()
+            var flipdown = new FlipDown(1631422799, 'contadorCyberMobile').start()
         },
         pago_datalayer(error_detectado){
           window.dataLayer = window.dataLayer || [];
--- pages/interbank/index.vue
+++ pages/interbank/index.vue
@@ -458,8 +458,8 @@
       },
       methods: {
         contador(){
-            var flipdown2 = new FlipDown(1631163599, 'contadorCyberDesktop').start()
-            var flipdown = new FlipDown(1631163599, 'contadorCyberMobile').start()
+            var flipdown2 = new FlipDown(1631422799, 'contadorCyberDesktop').start()
+            var flipdown = new FlipDown(1631422799, 'contadorCyberMobile').start()
         },
         pago_datalayer(error_detectado){
           window.dataLayer = window.dataLayer || [];
