1e90f3752f8b7324f3f686ad770252367ff70c51
Walter Mayorga <walter.mayorga.a@gmail.com>
2021-10-15 09:51:06 -0500

    Extensión de fecha en la campania

--- pages/index.vue
+++ pages/index.vue
@@ -458,8 +458,8 @@ import FadeLoader from '@/components/loaders/FadeLoader'
         onSubmit(){
         },
         contador(){
-            var flipdown2 = new FlipDown(1634273999, 'contadorCyberDesktop').start()
-            var flipdown = new FlipDown(1634273999, 'contadorCyberMobile').start()
+            var flipdown2 = new FlipDown(1634619599, 'contadorCyberDesktop').start()
+            var flipdown = new FlipDown(1634619599, 'contadorCyberMobile').start()
         },
         pago_datalayer(error_detectado){
           window.dataLayer = window.dataLayer || [];
